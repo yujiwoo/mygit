a6e951287d8114bb4ceedb18875caeedad455179
dbwldn77 <dbwldn77@naver.com>
2021-09-28 12:00:37 +0900

    merge test base 생성

---
+++ 01_hello_git/src/com/kh/git/MergeTest.java
@@ -0,0 +1,12 @@
+package com.kh.git;
+
+public class MergeTest {
+
+	public void mergeMe() {
+	
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+	}
+}
