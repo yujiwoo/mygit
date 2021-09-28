12e647dbac6b6bbb02933a78038c5401377193bf
dbwldn77 <dbwldn77@naver.com>
2021-09-28 12:26:35 +0900

    [animal] x 222 3 4 로 변경!

--- 01_hello_git/src/com/kh/git/MergeTest.java
+++ 01_hello_git/src/com/kh/git/MergeTest.java
@@ -4,8 +4,7 @@ public class MergeTest {
 
 	public void mergeMe() {
 	
-		System.out.println(1);
-		System.out.println(2);
+		System.out.println(222);
 		System.out.println(3);
 		System.out.println(4);
 	}
